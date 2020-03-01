0a60f78b28645b4b383e6e714410b7f802049321
softmix <mindmix@gmail.com>
2020-03-01 15:34:11 +0100

    Initial commit

---
+++ background.js
@@ -0,0 +1,31 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.executeScript({"file":"imagetabs.js"});
+});
+
+chrome.extension.onMessage.addListener(function (images_to_check, sender) {
+  var images_to_open = new Array();
+  var checked_images = 0;
+
+  //console.dir(images_to_check);
+
+  images_to_check.forEach(function(url) {
+    chrome.history.getVisits({ "url": url }, function(visits) {
+      if (!visits || (visits.length == 0)) {
+        images_to_open.push(url);
+      }
+      checked_images++;
+      if (checked_images == images_to_check.length) {
+        openImages(images_to_open.slice(0, 50));
+      }
+    });
+  });
+});
+
+var openImages = function(images_to_open) {
+  //console.dir(images_to_open);
+  images_to_open.forEach(function(url) {
+    chrome.tabs.create({'url': url, "selected": false}, function(tab) {
+      // Tab opened.
+    });
+  });
+}
---
+++ imagetabs.js
@@ -0,0 +1,24 @@
+var imagesregex = new RegExp("://[^/]*\.?((whatisnsfw|slutcapades|imagebam|babenudity|imagefap|motherless|nsfwpost|ultbabes|thiswebsiterules|phapz|phapit|flickr|imgur|drunkenstepfather|twimg|cdninstagram)\.com|(juvo)\.se/(\w{4})|(pomf)\.is|(1339)\.cf|(maxfile)\.ro|(subimg)\.net|(neud)\.org|(assvsart).\info)|(steamy)\.moe|\.(jpg|jpeg|gif|png|bmp|webm|mp4)$", "i");
+var images_to_check = new Array();
+var links = document.getElementsByTagNameNS("*", "a");
+
+if (links && links.length) {
+  var len = links.length;
+  for (var i = 0; i < len; ++i) {
+    var u = links[i].href;
+    if (u.substr(0, 4) == "http" && u.slice(-3) != "xpi" && u.slice(-4) != "m3u8" && imagesregex.test(u) && !inArray(u, images_to_check)) {
+      images_to_check.push(u);
+    }
+  }
+  chrome.extension.sendMessage(images_to_check);
+} else {
+  alert("Cant access links in document!", 1);
+}
+
+function inArray(needle, haystack) {
+  var length = haystack.length;
+  for(var i = 0; i < length; i++) {
+    if(haystack[i] == needle) return true;
+  }
+  return false;
+}
