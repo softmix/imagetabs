26e92d0f0ab5b6dbe59ab65caaa5421802e3ef70
softmix <mindmix@gmail.com>
2023-10-28 13:54:39 +0200

    fix scoping issue
    got constant redefinition on re-runs

--- imagetabs.js
+++ imagetabs.js
@@ -1,59 +1,61 @@
-const domains = [
-  "imgur.com",
-  "i.imgur.com",
-  "twimg.com",
-  "cdninstagram.com",
-  "gfycat.com",
-  "redgifs.com",
-  "jiggie.fun",
-];
+(function () {
+  const domains = [
+    "imgur.com",
+    "i.imgur.com",
+    "twimg.com",
+    "cdninstagram.com",
+    "gfycat.com",
+    "redgifs.com",
+    "jiggie.fun",
+  ];
 
-const fileExtensions = [
-  "jpg",
-  "jpeg",
-  "gif",
-  "png",
-  "bmp",
-  "webm",
-  "mp4",
-];
+  const fileExtensions = [
+    "jpg",
+    "jpeg",
+    "gif",
+    "png",
+    "bmp",
+    "webm",
+    "mp4",
+  ];
 
-const exclusions = [
-  "xpi",
-  "m3u8",
-]
+  const exclusions = [
+    "xpi",
+    "m3u8",
+  ]
 
-function startsWithHttp(url) {
-  return url.startsWith("http");
-}
+  function startsWithHttp(url) {
+    return url.startsWith("http");
+  }
 
-function isExcluded(url) {
-  return exclusions.some((exclusion) => url.endsWith(exclusion));
-}
+  function isExcluded(url) {
+    return exclusions.some((exclusion) => url.endsWith(exclusion));
+  }
 
-function hasMatchingDomain(url) {
-  return domains.some((domain) => url.includes(domain));
-}
+  function hasMatchingDomain(url) {
+    return domains.some((domain) => url.includes(domain));
+  }
 
-function hasMatchingExtension(url) {
-  return fileExtensions.some((extension) => url.endsWith(extension));
-}
+  function hasMatchingExtension(url) {
+    return fileExtensions.some((extension) => url.endsWith(extension));
+  }
 
-var images_to_check = new Array();
-var links = document.getElementsByTagNameNS("*", "a");
+  const images_to_check = [];
+  const links = document.getElementsByTagNameNS("*", "a");
 
-if (links && links.length) {
-  for (let link of links) {
-    const url = link.href;
-    if (startsWithHttp(url) && !isExcluded(url)) {
-      if (hasMatchingDomain(url) || hasMatchingExtension(url)) {
-        if (!images_to_check.includes(url)) {
-          images_to_check.push(url);
+  if (links && links.length) {
+    for (let link of links) {
+      const url = link.href;
+      if (startsWithHttp(url) && !isExcluded(url)) {
+        if (hasMatchingDomain(url) || hasMatchingExtension(url)) {
+          if (!images_to_check.includes(url)) {
+            images_to_check.push(url);
+          }
         }
       }
     }
+    browser.runtime.sendMessage(images_to_check);
+  } else {
+    alert("Cant access links in document!", 1);
   }
-  browser.runtime.sendMessage(images_to_check);
-} else {
-  alert("Cant access links in document!", 1);
-}
+})();
