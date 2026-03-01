7ff634c29850a028096f4d6bf91e2312ab077d2a
softmix <4247861+softmix@users.noreply.github.com>
2026-03-01 15:59:41 +0100

    convert onInstalled to async/await

--- background.js
+++ background.js
@@ -38,66 +38,54 @@ browser.runtime.onMessage.addListener(async (images_to_check, _sender) => {
   }
 });
 
-browser.runtime.onInstalled.addListener(function (details) {
-  if (details.reason === "install" || details.reason === "update") {
-    const defaultSettings = {
-      includedProtocols: ["http:", "https:"],
-      includedDomains: [
-        "imgur.com",
-        "i.imgur.com",
-        "twimg.com",
-        "cdninstagram.com",
-        "gfycat.com",
-        "redgifs.com",
-        "jiggie.fun",
-        "puzzle.aggie.io",
-        "catbox.moe",
-      ],
-      includedExtensions: [
-        "jpg",
-        "jpeg",
-        "gif",
-        "png",
-        "bmp",
-        "webm",
-        "mp4",
-      ],
-      excludedDomains: [
-        "saucenao.com",
-        "imgops.com",
-        "iqdb.org",
-        "google.com",
-      ],
-      excludedExtensions: [
-        "xpi",
-        "m3u8",
-      ],
-    };
+browser.runtime.onInstalled.addListener(async (details) => {
+  if (details.reason !== "install" && details.reason !== "update") return;
 
+  const defaultSettings = {
+    includedProtocols: ["http:", "https:"],
+    includedDomains: [
+      "imgur.com",
+      "i.imgur.com",
+      "twimg.com",
+      "cdninstagram.com",
+      "gfycat.com",
+      "redgifs.com",
+      "jiggie.fun",
+      "puzzle.aggie.io",
+      "catbox.moe",
+    ],
+    includedExtensions: [
+      "jpg",
+      "jpeg",
+      "gif",
+      "png",
+      "bmp",
+      "webm",
+      "mp4",
+    ],
+    excludedDomains: [
+      "saucenao.com",
+      "imgops.com",
+      "iqdb.org",
+      "google.com",
+    ],
+    excludedExtensions: [
+      "xpi",
+      "m3u8",
+    ],
+  };
 
-    const keys = Object.keys(defaultSettings);
-    browser.storage.sync.get(keys, (currentSettings) => {
-      if (browser.runtime.lastError) {
-        console.error(`Error retrieving settings: ${browser.runtime.lastError}`);
-        return;
-      }
+  const keys = Object.keys(defaultSettings);
+  const currentSettings = await browser.storage.sync.get(keys);
 
-      const settingsToSet = {};
-      keys.forEach((key) => {
-        if (currentSettings[key] === undefined) {
-          settingsToSet[key] = defaultSettings[key];
-        }
-      });
+  const settingsToSet = {};
+  for (const key of keys) {
+    if (currentSettings[key] === undefined) {
+      settingsToSet[key] = defaultSettings[key];
+    }
+  }
 
-      if (Object.keys(settingsToSet).length > 0) {
-        browser.storage.sync.set(settingsToSet, () => {
-          if (browser.runtime.lastError) {
-            console.error(`Error setting default settings: ${browser.runtime.lastError}`);
-          } else {
-            console.log("Default settings set successfully");
-          }
-        });
-      }
-    });
+  if (Object.keys(settingsToSet).length > 0) {
+    await browser.storage.sync.set(settingsToSet);
   }
 });
